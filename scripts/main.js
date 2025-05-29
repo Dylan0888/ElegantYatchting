0a4c679f4d47141d1a59999f00a8b30708c8a885
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2025-05-29 18:12:01 +0100

    Created nav, with styles. Created a welcome message content section with styles. loaded the img into a div using background image and added title, phrase and button onto the image

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// atempting to create a mehtod of dimming other elements in a nav when hovering over once spasific link 
+
+/*
+    possible through adding a div class to each of the nav items when hovering over one 
+
+    then for the spasfiic one remvove the dim and highlight 
+
+    then remove the the hihglight and dim from all when exiting the hover 
+
+
+*/
+
+// const navLinks = document.querySelectorAll('.js-nav-li')
+
+// // navLinks.forEach((link, index) => {
+// //     //console.log(link, index);
+// //     link.addEventListener("mouseover", ()=>{
+// //         console.log(link)
+// //         link.style.color = "blue";
+// //     })
+// //     link.style.color = "black";
+// // });
+
+
+
+// console.log(navLinks);
