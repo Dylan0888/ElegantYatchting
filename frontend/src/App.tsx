3cddf59359890d835fca1a385b6afab11cc17838
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-03-26 13:45:54 +0000

    Attempting to modify code for scalability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import Nav from "./components/Nav"
+import Carousel from "./components/homePageComponents/Carousel"
 
 function App() {
+
+ 
+
+  const images: [
+      "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp",
+      "https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp",
+      "https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp",
+      "https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+    ]
+  }
 
 
   return (
     <>
       <Nav />
+      <Carousel images={images}/>
     </>
   )
 }
--- frontend/src/components/homePageComponents/Carousel.tsx
+++ frontend/src/components/homePageComponents/Carousel.tsx
@@ -8,11 +8,26 @@ const Carousel = ({images}: CarouselProps) => {
     return (
         <>
             <div className="carousel w-full">
+
+                {
+                    images.map((src, index) => (
+                        <div key={index} id={`item${index + 1}`} className="carousel-item w-full">
+                            <img src={src} className="w-full" />
+                        </div>
+                    ))
+                }
+
+
                 <div id="item1" className="carousel-item w-full">
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
                         className="w-full" />
                 </div>
+               
+               
+               
+               
+               
                 <div id="item2" className="carousel-item w-full">
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
@@ -28,7 +43,21 @@ const Carousel = ({images}: CarouselProps) => {
                         src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
                         className="w-full" />
                 </div>
+
+
+
             </div>
+
+
+
+
+
+
+
+
+
+
+
             <div className="flex w-full justify-center gap-2 py-2">
                 <a href="#item1" className="btn btn-xs">1</a>
                 <a href="#item2" className="btn btn-xs">2</a>
