26f1203bb58f322b3649e7342c3024080f6fad8c
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-03-26 13:56:16 +0000

    Optimized the code with scalability in mind allowing the Carousel to have as many images as desired.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,13 @@ function App() {
 
  
 
-  const images: [
+  const images = [
       "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp",
       "https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp",
       "https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp",
       "https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
     ]
-  }
-
-
+  
   return (
     <>
       <Nav />
@@ -21,5 +19,4 @@ function App() {
     </>
   )
 }
-
 export default App
