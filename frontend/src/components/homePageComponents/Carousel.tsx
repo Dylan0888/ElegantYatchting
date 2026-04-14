ab7e814dfea52cfd8881bbf5be58150d459e766b
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-04-14 11:45:36 +0100

    changed the switching design for the buttons. Removed bottom buttons and replaced with buttons on left and right side of the carousel

--- frontend/src/components/homePageComponents/Carousel.tsx
+++ frontend/src/components/homePageComponents/Carousel.tsx
@@ -1,35 +1,73 @@
-import React from "react";
+import { useState } from "react";
 
 interface CarouselProps {
   images: string[];
 }
 
 const Carousel = ({ images }: CarouselProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goLeft = () => {
+    setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const goRight = () => {
+    setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
   return (
-    <>
-      <div className="carousel w-full">
-        {images.map((src, index) => (
-          <div
-            key={index}
-            id={`item${index + 1}`}
-            className="carousel-item w-full"
-          >
-            <img src={src} className="w-full" />
-          </div>
-        ))}
+    <div className="relative w-full ">
+      {/* Image */}
+      <div className="w-full rounded-2xl overflow-hidden">
+        <img
+          src={images[currentIndex]}
+          className="w-full"
+          alt={`Slide ${currentIndex}`}
+        />
       </div>
 
-      <div className="flex w-full justify-center gap-2 py-2">
-        {images.map((_, index) => (
-          <a key={index} 
-             href={`#item${index + 1}`} 
-             className="btn btn-xs rounded-lg border-neutral-500 text-neutral-500 hover:bg-neutral-500 hover:text-white"
-            >
-            {index + 1}
-          </a>
-        ))}
-      </div>
-    </>
+      {/* Left Button */}
+      <button
+        onClick={goLeft}
+        className="absolute left-0 top-0 h-full w-[10%] cursor-pointer
+             flex items-center justify-center
+             bg-gray-700/40 text-white text-2xl rounded-2xl backdrop-blur-sm
+             border border-black/60
+             opacity-0 hover:opacity-100 
+             transition-all duration-300
+             hover:shadow-[8px_0_15px_rgba(0,0,0,0.4)]"
+      >
+        ❮
+      </button>
+
+      {/* Right Button */}
+      <button
+        onClick={goLeft}
+        className="absolute left-0 top-0 h-full w-[10%] cursor-pointer
+             flex items-center justify-center
+             bg-gray-700/40 text-white text-2xl rounded-2xl backdrop-blur-sm
+             border border-black/60
+             opacity-0 hover:opacity-100 
+             transition-all duration-300
+             hover:shadow-[8px_0_15px_rgba(0,0,0,0.4)]"
+      >
+        ❮
+      </button>
+
+      {/* Right Button */}
+      <button
+        onClick={goRight}
+        className="absolute right-0 top-0 h-full w-[10%] cursor-pointer
+             flex items-center justify-center
+             bg-gray-700/40 text-white text-2xl rounded-2xl backdrop-blur-sm
+             border border-black/60
+             opacity-0 hover:opacity-100 
+             transition-all duration-300
+             hover:shadow-[-8px_0_15px_rgba(0,0,0,0.4)]"
+      >
+        ❯
+      </button>
+    </div>
   );
 };
 
