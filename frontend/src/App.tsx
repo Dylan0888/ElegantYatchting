52d91c7fac8013e8594ad4f6be560d6a07dad1f5
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-05-02 17:07:35 +0100

    Removed icon testing from home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import Nav from "./components/Nav"
 import Carousel from "./components/homePageComponents/Carousel"
 import CarouselFooter from "./components/homePageComponents/CarouselFooter"
 import {carouselSet} from "./assets/carousellSet"
-import icon from "./assets/imgs/icon.png"
 
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
       <section>
         <Carousel images={carouselSet}/>
         <CarouselFooter/>
-        <img src={icon} alt="Elegant Yachting Icon" className="w-[80px]" />
+       
 
       </section>
       
