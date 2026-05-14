5edb6b5802b53611e1ba6bc8c662c4ed4ea43a62
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-05-14 13:33:01 +0100

    Configured CSS for yacht card.

--- frontend/src/components/homePageComponents/YachtFleetComponents/YachtCard.tsx
+++ frontend/src/components/homePageComponents/YachtFleetComponents/YachtCard.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FaSailboat } from "react-icons/fa6";
+import { PiPersonSimpleSwimBold } from "react-icons/pi";
+import { BsPersonFill } from "react-icons/bs";
 
 const YachtCard = () => {
     return (
-        <div className="card bg-base-100 w-96 shadow-sm    m-2">
+        <div className="card bg-base-100 w-96 shadow-sm border border-text-secondary rounded-md cursor-pointer hover:shadow-lg transition-shadow duration-300">
             <figure>
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -11,13 +14,20 @@ const YachtCard = () => {
             <div className="card-body">
                 <h2 className="card-title">
                     Yacht Name
-                    <div className="badge badge-secondary">NEW</div>
+                    <div className="badge badge-secondary px-1 rounded-md bg-gold text-text-primary">NEW</div>
                 </h2>
-                {/* <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
-                </div> */}
+                <p className='text-text-secondary'>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions justify-between text-text-secondary">
+                    <div className="badge badge-outline">
+                        <FaSailboat className='text-gold' />100m
+                    </div>
+                    <div className="badge badge-outline">
+                        <BsPersonFill className='text-gold' />10 Guests
+                    </div>
+                    <div className="badge badge-outline">
+                        <PiPersonSimpleSwimBold className='text-gold' />100 Crew
+                    </div>
+                </div>
             </div>
         </div>
     )
