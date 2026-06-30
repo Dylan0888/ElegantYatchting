eed361e832602174f4834f680db14e4174a9d6e8
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-06-30 21:38:25 +0100

    Created a file to store the details for the awards. Will be used for awards container component

---
+++ frontend/src/assets/AwardsList.ts
@@ -0,0 +1,54 @@
+import type {Awards} from "../types/types";
+import badge1 from "./imgs/awardImages/badge1.png";
+import badge2 from "./imgs/awardImages/badge2.png";
+import badge3 from "./imgs/awardImages/badge3.png";
+import badge4 from "./imgs/awardImages/badge4.png";
+import badge5 from "./imgs/awardImages/badge5.png";
+import badge6 from "./imgs/awardImages/badge6.png";
+
+
+export const awardList: Awards = [
+    {
+        img: `${badge1}`,
+        alt: "2025 Award Badge",
+        title: "Best Luxury Yacht Charter Company 2025",
+        description: "Elegant Yachting has been recognized as the Best Luxury Yacht Charter Company for 2025, showcasing our commitment to excellence in providing unforgettable yachting experiences.",
+        provider: "Global Luxury Awards" 
+    },
+    {
+        img: `${badge2}`,
+        alt: "2026 Award Badge",
+        title: "Best Luxury Yacht Charter Company 2026",
+        description: "Elegant Yachting has been recognized as the Best Luxury Yacht Charter Company for 2026, showcasing our commitment to excellence in providing unforgettable yachting experiences.",
+        provider: "Global Luxury Awards"
+    },
+    {
+        img: `${badge3}`,
+        alt: "Acredited Character Award Badge",
+        title: "Accredited Character",
+        description: "Elegant Yachting has been recognized for its Accredited Character, demonstrating our dedication to maintaining the highest standards of integrity and professionalism in the yachting industry.",
+        provider: "Global Luxury Awards"
+    },
+    {
+        img: `${badge4}`,
+        alt: "Elite Odyssey Character Award Badge",
+        title: "Elite Odyssey Character",
+        description: "Elegant Yachting has been recognized for its Elite Odyssey Character, showcasing our commitment to providing exceptional yachting experiences and maintaining the highest standards of service.",
+        provider: "Global Luxury Awards"
+    },
+    {
+        img: `${badge5}`,
+        alt: "Royal Voyage Character Award Badge",
+        title: "Royal Voyage Character",
+        description: "Elegant Yachting has been recognized for its Royal Voyage Character, highlighting our dedication to delivering unparalleled luxury and service in the yachting industry.",
+        provider: "Global Luxury Awards"
+    },
+    {
+        img: `${badge6}`,
+        alt: "Serenity Odyssey Character Award Badge",
+        title: "Serenity Odyssey Character",
+        description: "Elegant Yachting has been recognized for its Serenity Odyssey Character, reflecting our commitment to providing serene and luxurious yachting experiences for our esteemed clients.",
+        provider: "Global Luxury Awards"
+    }
+   
+]
