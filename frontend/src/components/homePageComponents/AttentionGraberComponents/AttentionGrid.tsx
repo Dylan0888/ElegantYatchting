ca2e5395196618d6acae9161aa6f32a62e35dfb3
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-07-07 14:48:49 +0100

    Altered the type for the attention grabbers to include button text and changed the family cruise image .

--- frontend/src/components/homePageComponents/AttentionGraberComponents/AttentionGrid.tsx
+++ frontend/src/components/homePageComponents/AttentionGraberComponents/AttentionGrid.tsx
@@ -1,11 +1,13 @@
 import CruiseType from "./CruiseType"
+import { cruiseAttentionGrabbers }  from "../../../assets/CruiseAttentionList"
 
 const AttentionGrid = () => {
     return (
         <div className="flex flex-col md:flex-row justify-center my-10 mx-5 
                     sm:items-center gap-10 lg:gap-20 xl:gap-30 ">
-            <CruiseType />
-            <CruiseType />
+            {cruiseAttentionGrabbers.map((cruise, index) => (
+                <CruiseType key={index} {...cruise} />
+            ))}
 
         </div>
     )
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -59,4 +59,5 @@ export type CruiseAttentionGrabber = {
     description: string
     image: string
     icon: IconType
+    buttonText: string
 }
