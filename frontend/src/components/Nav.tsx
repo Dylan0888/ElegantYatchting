678483b6551a32bcf93d598d0ae8707d4dfedb93
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-07-17 20:24:22 +0100

    updated nav file to include the link import and configured the span mapping though the nav link names to route the user to ach page.

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import icon from "../assets/imgs/icon.png";
 import { HiMenu, HiX } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const navItems = ["Home", "Tours", "Gallery", "About"];
+    const navItems = ["home", "tours", "gallery", "about"];
+
 
     return (
         <nav className="relative z-50 w-full px-6 py-3 
                     flex items-center justify-between
-                    bg-[var(--color-bg)] border-b border-[var(--color-border)]">
+                    bg-bg border-b border-border">
 
             {/* Logo */}
             <div className="flex items-center gap-2 cursor-pointer">
@@ -19,24 +22,27 @@ const Nav = () => {
                     alt="Elegant Yachting"
                     className="w-10 h-10 object-contain"
                 />
-                <span className="text-[var(--color-gold)] font-semibold text-xl sm:block font-elegant ">
+                <span className="text-gold font-semibold text-xl sm:block font-elegant ">
                     Elegant Yachting
                 </span>
             </div>
 
             {/* Desktop Menu */}
-            <ul className="hidden md:flex gap-8 text-[var(--color-text-primary)] font-medium">
+            <ul className="hidden md:flex gap-8 text-text-primary font-medium">
                 {navItems.map((item, index) => (
                     <li
                         key={index}
                         className="cursor-pointer relative group"
                     >
-                        <span className="hover:text-[var(--color-gold)] transition-colors duration-300">
-                            {item}
-                        </span>
+                        {/* Links to each page based on the nav item */}
+                        <Link to={`/${item}`}> 
+                            <button className="capitalize hover:text-gold transition-colors duration-300">
+                                {item}
+                            </button>
+                        </Link>
 
                         {/* underline animation */}
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[var(--color-gold)] 
+                        <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-gold 
                              transition-all duration-300 group-hover:w-full"></span>
                     </li>
                 ))}
@@ -44,7 +50,7 @@ const Nav = () => {
 
             {/* Hamburger Button (mobile only) */}
             <button
-                className="md:hidden text-[var(--color-text-primary)] text-2xl"
+                className="md:hidden text-text-primary text-2xl"
                 onClick={() => setIsOpen(!isOpen)}
             >
                 {isOpen ? <HiX /> : <HiMenu />}
@@ -53,8 +59,8 @@ const Nav = () => {
             {/* Mobile Menu */}
             <div
                 className={`absolute top-full left-0 w-full 
-                    bg-[var(--color-secondary-bg)] 
-                    border-b border-[var(--color-border)]
+                    bg-secondary-bg 
+                    border-b border-border
                     flex flex-col items-center gap-6 py-6
                     transition-all duration-300
                     ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}
@@ -62,8 +68,8 @@ const Nav = () => {
                 {navItems.map((item, index) => (
                     <span
                         key={index}
-                        className="text-[var(--color-text-primary)] text-lg cursor-pointer 
-                       hover:text-[var(--color-gold)] transition-colors"
+                        className="text-text-primary text-lg cursor-pointer 
+                       hover:text-gold transition-colors"
                         onClick={() => setIsOpen(false)}
                     >
                         {item}
