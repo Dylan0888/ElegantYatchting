9719a6f974482fa28035040fc5c4692b1f32132a
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-07-28 15:55:22 +0100

    Started working on filter options

--- frontend/src/components/galleryPageComponents/FilterContainer.tsx
+++ frontend/src/components/galleryPageComponents/FilterContainer.tsx
@@ -1,6 +1,48 @@
 import FilterButton from "./FilterButton";
+import type { GalleryItemStructure } from "../../types/types";
+interface props {
+  data: GalleryItemStructure[],
+  setList: React.Dispatch<React.SetStateAction<GalleryItemStructure[]>>
+}
 
-const FilterContainer = () => {
+// interface filterButtons {
+//   name: string
+//   func: function name(params:type) {
+
+//   }
+// }
+
+const FilterContainer = ({ data, setList }: props) => {
+
+  const dateSortOptions = [
+  {
+    label: "Most Recent Voyage",
+    func: (d: GalleryItemStructure[]) =>
+      [...d].sort(
+        (a, b) =>
+          new Date(b.date).getTime() -
+          new Date(a.date).getTime()
+      ),
+  },
+  {
+    label: "Earliest Voyage",
+    func: (d: GalleryItemStructure[]) =>
+      [...d].sort(
+        (a, b) =>
+          new Date(a.date).getTime() -
+          new Date(b.date).getTime()
+      ),
+  },
+];
+
+
+
+
+
+
+
+
+
   return (
     <section className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-4 px-6 py-10">
       <FilterButton title="Location" />
--- frontend/src/pages/GalleryPage.tsx
+++ frontend/src/pages/GalleryPage.tsx
@@ -5,17 +5,23 @@ import FilterContainer from '../components/galleryPageComponents/FilterContainer
 import Hero from '../components/galleryPageComponents/Hero'
 import data from '../assets/data/gallery/imageData.json'
 import type { GalleryItemStructure } from '../types/types'
+import { useState } from 'react'
 
 const GalleryPage = () => {
   const imgData: GalleryItemStructure[] = data
   
+  const [imgList, setImageList] = useState<GalleryItemStructure[]>(imgData)
+
+
+
+
   return (
    <>
     <Nav />
     <Hero/>
     <div className="gallery-page">
-        <FilterContainer/>
-        <GalleryGrid data={imgData}/>
+        <FilterContainer data={imgList} setList={setImageList}/>
+        <GalleryGrid data={imgList}/>
     </div>
     <Footer />
   
