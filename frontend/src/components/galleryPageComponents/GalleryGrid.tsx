d57714422452dbef72d5a832bb78fdbc54f2e615
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-07-28 11:13:20 +0100

    Updated the tailwind for grid item component

--- frontend/src/components/galleryPageComponents/GalleryGrid.tsx
+++ frontend/src/components/galleryPageComponents/GalleryGrid.tsx
@@ -3,15 +3,15 @@ import GridItem from './GridItem'
 
 const GalleryGrid = () => {
   return (
-    <div className='flex flex-wrap gap-5 justify-evenly'>
+    <section className='flex flex-wrap gap-5 justify-evenly'>
         <GridItem/>
         <GridItem/>
         <GridItem/>
         <GridItem/>
         <GridItem/>
         <GridItem/>
-      
-    </div>
+  
+    </section>
   )
 }
 
--- frontend/src/components/galleryPageComponents/GridItem.tsx
+++ frontend/src/components/galleryPageComponents/GridItem.tsx
@@ -1,37 +1,75 @@
-import React from 'react'
-import GridIcon from './GridIcon';
+import GridIcon from "./GridIcon";
 
 import { FaLocationDot } from "react-icons/fa6";
 import { MdCalendarMonth } from "react-icons/md";
 
 const GridItem = () => {
-
-    // each item should have :
-    // image src + alt
-
-    // -- These will be filterable --
-    // date taken 
-    // location taken 
-    // filter tags 
-
-  
-
     return (
-        <div tabIndex={0} className="card flex border-1 shadow-lg  w-80 md:w-90 md:border-red-400 lg:w-100 lg:border-amber-200">
-            <figure className='object-cover'>
+        <article
+            className="
+        group
+        w-70
+        overflow-hidden
+        rounded-xl
+        border-2
+        border-border
+        bg-secondary-bg
+        shadow-xl
+        transition-all
+        duration-500
+        hover:-translate-y-1
+        hover:border-gold
+        hover:shadow-2xl
+      "
+        >
+            {/* Image */}
+
+            <div className="relative overflow-hidden">
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    alt="Movie" />
-            </figure>
-            <div className="  h-8 text-sm  ">
-                <div className='flex justify-evenly p-1'>
-                    <GridIcon Icon={FaLocationDot} text={"New Zealand"}/>
-                    <GridIcon Icon={MdCalendarMonth} text={"18/20/2018"}/>         
-                </div>
+                    alt="Luxury yacht cruising through New Zealand"
+                    className="
+            h-80
+            w-full
+            object-cover
+            transition-transform
+            duration-700
+            group-hover:scale-110
+          "
+                />
 
+                {/* Dark overlay */}
+
+                <div className="absolute inset-0 bg-black/30"/>
+
+                {/* Tags */}
+
+                {/* <div className="absolute left-4 top-4 flex gap-2">
+          <span className="rounded-full bg-black/50 px-3 py-1 text-xs text-white backdrop-blur-md">
+            Scenic
+          </span>
+
+          <span className="rounded-full bg-gold px-3 py-1 text-xs font-medium text-bg">
+            Luxury
+          </span>
+        </div> */}
             </div>
-        </div>
-    )
-}
 
-export default GridItem
+            {/* Footer */}
+
+            <div className="space-y-4 p-5">
+                <h3 className="text-lg font-semibold text-text-primary">
+                    Milford Sound
+                </h3>
+
+                <div className="flex justify-between text-sm text-text-secondary">
+                    <GridIcon Icon={FaLocationDot} text="New Zealand" />
+
+                    <GridIcon Icon={MdCalendarMonth} text="18 May 2026" />
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default GridItem;
