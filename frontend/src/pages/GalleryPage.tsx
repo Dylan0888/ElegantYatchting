d423504ba2b012dec099fd36ad1e7b867ebff649
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-08-01 19:38:23 +0100

    Simplified the code to remove the useEffect and useState for the filtered lists and use UseMemo instead.

--- frontend/src/pages/GalleryPage.tsx
+++ frontend/src/pages/GalleryPage.tsx
@@ -5,13 +5,13 @@ import FilterContainer from '../components/galleryPageComponents/FilterContainer
 import Hero from '../components/galleryPageComponents/Hero'
 import data from '../assets/data/gallery/imageData.json'
 import type { GalleryItemStructure } from '../types/galleryTypes'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import type {filters} from '../types/galleryTypes'
 
 const GalleryPage = () => {
   const imgData: GalleryItemStructure[] = data
   
-  const [imgList, setImageList] = useState<GalleryItemStructure[]>(imgData)
+  // const [imgList, setImageList] = useState<GalleryItemStructure[]>(imgData)
 
   const [selectedFilters, setFilters] = useState<filters>({
     location: "",
@@ -21,28 +21,47 @@ const GalleryPage = () => {
 
   // Packs filter state for props 
   const filterState ={
-    selectedFilters: selectedFilters,
-    setFilters: setFilters
+    selectedFilters,
+    setFilters
   }
 
+  
+  // Memoized filtered image list based on selected filters
+  const imgList = useMemo((): GalleryItemStructure[] => {
+    let filteredList = imgData
 
- // Based on selected filters change, filter the image list to only show images that match the selected filters
-  // use a use effect to watch for changes in the selectedFilters state and update the imgList state accordingly
+    if (selectedFilters.location) {
+      filteredList = filteredList.filter((item) => item.location === selectedFilters.location)
+    }
+
+    if (selectedFilters.date) {
+      filteredList = filteredList.filter((item) => item.date === selectedFilters.date)
+    }
+
+    if (selectedFilters.tags) {
+      filteredList = filteredList.filter((item) => item.tags.includes(selectedFilters.tags))
+    }
+    return filteredList
+
+  }, [selectedFilters, imgData])
+
+
+  // do some form of checking to grey out or disable filter buttons that have no results in the current filtered list.
+  
 
 
 
 
   return (
-   <>
-    <Nav />
-    <Hero/>
-    <div className="gallery-page">
-        <FilterContainer data={imgList} filterState={filterState}/>
-        <GalleryGrid data={imgList}/>
-    </div>
-    <Footer />
-  
-   </>
+    <>
+      <Nav />
+      <Hero />
+      <div className="gallery-page">
+        <FilterContainer data={imgData} filterState={filterState} />
+        <GalleryGrid data={imgList} />
+      </div>
+      <Footer />
+    </>
   )
 }
 
