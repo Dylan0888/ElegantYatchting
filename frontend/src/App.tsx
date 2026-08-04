cce42550870532c55150c66b55b004afbb15f506
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-08-04 11:09:00 +0100

    removed testing imports and set the home page as landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import HomePage from "./pages/HomePage"
-import AboutPage from "./pages/AboutPage"
-import GalleryPage from "./pages/GalleryPage"
-import ToursPage from "./pages/ToursPage"
 
 
 function App() {  
   return (
     <>
-      <ToursPage/>
+      <HomePage/>
     </>
   )
 }
